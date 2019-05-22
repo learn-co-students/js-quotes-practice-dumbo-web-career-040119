afecd1d49fdd6fa1636d67ff77df399e7dba8eec
Mallory Woods <malvwoods@gmail.com>
2019-05-21 20:26:55 -0400

    added update feature

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ document.addEventListener("DOMContentLoaded", () =>{
   const newQuoteForm = document.querySelector('#new-quote-form')
   const quoteList = document.querySelector('#quote-list')
 
+
   quoteList.addEventListener("click", (event) => {
     if (event.target.className === 'btn-danger') {
       deleteQuote(event.target.parentNode.parentNode)
     } else if (event.target.className === 'btn-success') {
       increaseLikes(event.target, event.target.parentNode.parentNode)
+    } else if (event.target.className === 'btn-update') {
+
+      showUpdateForm(event)
     }
   })
 
@@ -34,6 +38,7 @@ document.addEventListener("DOMContentLoaded", () =>{
       <br>
       <button class='btn-success'>Likes: <span>${quote.likes}</span></button>
       <button class='btn-danger'>Delete</button>
+      <button class='btn-update'>Update</button>
     </blockquote>`
 
     quoteList.append(newQuoteLi)
@@ -83,5 +88,47 @@ document.addEventListener("DOMContentLoaded", () =>{
     })
     .then(response => response.json())
     .then(button.children[0].innerText = newNum)
+  }
+
+  function showUpdateForm(event){
+    // stopped here - need to finish figuring out how to populate field
+    const updateForm = document.createElement('form')
+    updateForm.className = 'update-form'
+    updateForm.innerHTML = `<div class="form-group">
+      <label for="quote">Quote</label>
+      <input type="text" class="form-control" id="update-quote">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>`
+    event.target.parentElement.parentElement.append(updateForm)
+    updateForm.addEventListener("submit", function(event){
+      event.preventDefault()
+      updateQuote(event)
+    })
+  }
+
+  function updateQuote(event){
+    const id = event.target.parentElement.dataset.quoteId
+    const oldQuote = event.target.parentElement.querySelector('p')
+    const form = document.querySelector('.update-form')
+    let newQuote = event.target[0].value
+    let author = event.target[1].value
+
+    fetch(`http://localhost:3000/quotes/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        quote: newQuote
+      })
+    })
+    .then(resp => resp.json())
+    .then(quote => oldQuote.innerText = newQuote)
+    // remove form from dom
+    .then(form.remove())
+
   }
 })
